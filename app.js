496923233d548f76ff16ec198a2561cbc2b99bef
Eliana <crisely.cye@gmail.com>
2023-03-12 01:28:34 -0300

    Carpeta de eventos creada

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const fs = require('fs')
 const path = require('path')
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const { Client, Collection, GatewayIntentBits } = require('discord.js')
 const dotenv = require('dotenv'); dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
+// Para que funcionen los eventos :c
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file)
+  const event = require(filePath)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
+
+// Para que funcionen los comandos :c
 client.commands = new Collection()
+
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
@@ -19,31 +35,5 @@ for (const file of commandFiles) {
     console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
   }
 }
-
-client.on(Events.ClientReady, () => {
-  console.log('Ready!')
-})
-
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return
-
-  const command = interaction.client.commands.get(interaction.commandName)
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`)
-    return
-  }
-
-  try {
-    await command.execute(interaction)
-  } catch (error) {
-    console.error(error)
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
-    } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-    }
-  }
-})
 
 client.login(process.env.TOKEN)
