5a17f072c332b5bb6fc179c935042017fe286a23
Eliana <crisely.cye@gmail.com>
2023-03-28 00:27:49 -0300

    Cambios

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,14 +8,14 @@ module.exports = {
     const command = interaction.client.commands.get(interaction.commandName)
 
     if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`)
+      console.error(`No se encontró ningún comando que coincida con ${interaction.commandName}`)
       return
     }
 
     try {
       await command.execute(interaction)
     } catch (error) {
-      console.error(`Error executing ${interaction.commandName}`)
+      console.error(`Error al ejecutar ${interaction.commandName}`)
       console.error(error)
     }
   }
